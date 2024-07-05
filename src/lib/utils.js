87bb1cddffbffdf02644a5dc92bef6c0269aa337
Vukasin Paunovic <vukasinpaunovich@gmail.com>
2024-07-05 14:06:03 +0200

    Add functions for url check, url byte length check, url regex and open in new tab

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,8 @@
 import { format } from 'date-fns';
 
+export const URL_REGEX =
+    /^(?:(?:https?:\/\/)?(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})(?:\/[^\s]*)?)|(?:ipfs:\/\/[a-f0-9]+(?:\/[a-zA-Z0-9_]+)*)$|^$/;
+
 export const formatIsoDate = (isoDate) => {
     if (!isoDate) return '';
 
@@ -40,4 +43,25 @@ export const utf8ToHex = (str) => {
     return Array.from(str)
         .map((char) => char.charCodeAt(0).toString(16).padStart(2, '0'))
         .join('');
+};
+
+export function isValidURLFormat(str) {
+    if (!str.length) return false;
+    return URL_REGEX.test(str);
+}
+
+export function isValidURLLength(s) {
+    if (s.length > 128) {
+        return 'Url must be less than 128 bytes';
+    }
+
+    const encoder = new TextEncoder();
+    const byteLength = encoder.encode(s).length;
+
+    return byteLength <= 128 ? true : 'Url must be less than 128 bytes';
+}
+
+export const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
 };
